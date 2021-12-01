4c55f9850f1eda88bc4c4508383c9d1e5a620fa5
OWDSC <oliver@familie-wieder.de>
2021-12-01 14:25:27 +0100

    spacy test

--- src/SeemsPhishy/nlp.py
+++ src/SeemsPhishy/nlp.py
@@ -1,6 +1,34 @@
 import spacy
 from spacy.tokens import Doc
 
+nlp = spacy.load("en_core_web_sm")
+
 doc = nlp.make_doc("This is a sentence")
 for name, proc in nlp.pipeline:          
-    doc = proc(doc)        
+    doc = proc(doc)     
+
+print(doc)   
+
+#python -m spacy download en_core_web_sm (muss vor Ausführung installiert werden)
+
+# import spacy
+# from spacy.tokens import Doc
+
+# if not Doc.has_extension("text_id"):
+#     Doc.set_extension("text_id", default=None)
+
+# text_tuples = [
+#     ("This is the first text.", {"text_id": "text1"}),
+#     ("This is the second text.", {"text_id": "text2"})
+# ]
+
+# nlp = spacy.load("en_core_web_sm")
+# doc_tuples = nlp.pipe(text_tuples, as_tuples=True)
+
+# docs = []
+# for doc, context in doc_tuples:
+#     doc._.text_id = context["text_id"]
+#     docs.append(doc)
+
+# for doc in docs:
+#     print(f"{doc._.text_id}: {doc.text}")
