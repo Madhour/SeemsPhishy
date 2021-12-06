a5b4a14450fad960201b1aad1bab2ffdb2fe1d2b
BennerLukas <arbeit.protonmail.com>
2021-12-06 14:45:19 +0100

    working flask pages (empty)

--- src/SeemsPhishy/gui/app/__init__.py
+++ src/SeemsPhishy/gui/app/__init__.py
@@ -7,3 +7,5 @@ app = Flask(__name__, instance_relative_config=True)
 
 # Load the config file
 app.config.from_object('config')
+
+from src.SeemsPhishy.gui.app import views
--- src/SeemsPhishy/gui/app/views.py
+++ src/SeemsPhishy/gui/app/views.py
@@ -1,10 +1,79 @@
 from flask import render_template, request, session
-import psycopg2
+from src.SeemsPhishy.gui.app import app
 
-from src.SeemsPhishy.gui import app
+
+app.secret_key = 'dljsawadslqk24e21cjn!Ew@@dsa5'
 
 
 @app.route('/')  # Home
 def index():
+    return render_template("/index.html")
 
-    return "hello world"
+
+@app.route('/datasets')
+def datasets():
+    return render_template("/datasets_overview.html")
+
+
+@app.route('/datasets/companies')
+def datasets_companies():
+    return render_template("/datasets_companies.html")
+
+
+@app.route('/datasets/pdfs')
+def datasets_pdfs():
+    return render_template("/datasets_pdfs.html")
+
+
+@app.route('/text-generation/letter')
+def text_generation_letter():
+    return render_template("/text_generation_letter.html")
+
+
+@app.route('/text-generation/email')
+def text_generation_email():
+    return render_template("/text_generation_email.html")
+
+
+@app.route('/text-generation/newsletter')
+def text_generation_newsletter():
+    return render_template("/text_generation_newsletter.html")
+
+
+@app.route('/information-gain')
+def information_gain():
+    return render_template("/information_gain_overview.html")
+
+
+@app.route('/information-gain/companies')
+def information_gain_companies():
+    return render_template("/information_gain_companies.html")
+
+
+@app.route('/information-gain/leakage')
+def information_gain_leakage():
+    return render_template("/information_gain_leakage.html")
+
+#################################################################
+
+@app.route('/faq')
+def faq():
+    return render_template("/faq.html")
+
+
+@app.route('/settings')
+def settings():
+    return render_template("/settings.html")
+
+
+@app.route('/profile')
+def profile():
+    return render_template("/profile.html")
+
+#################################################################
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    return render_template("/datasets_companies.html")
+
