4851bd736cb355d04e1fb9ae37a4ead42810b0ee
OWDSC <oliver@familie-wieder.de>
2021-12-07 12:58:17 +0100

    NLP Keywords

--- src/SeemsPhishy/nlp.py
+++ src/SeemsPhishy/nlp.py
@@ -1,13 +1,14 @@
 import spacy
 from spacy.tokens import Doc
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
 
-doc = nlp.make_doc("This is a sentence")
-for name, proc in nlp.pipeline:          
-    doc = proc(doc)     
+text = """spaCy is an open-source software library for advanced natural language processing, 
+written in the programming languages Python and Cython. The library is published under the MIT license
+and its main developers are Matthew Honnibal and Ines Montani, the founders of the software company Explosion."""
+doc = nlp(text)   
 
-print(doc)   
+print(doc.ents)   
 
 #python -m spacy download en_core_web_sm (muss vor Ausführung installiert werden)
 
