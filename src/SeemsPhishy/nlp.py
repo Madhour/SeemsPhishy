53f109f9313514d5eab9926cf915d5b35028a60f
OWDSC <oliver@familie-wieder.de>
2021-12-07 17:28:00 +0100

    aus txt Datei Keywords auslesen

--- src/SeemsPhishy/nlp.py
+++ src/SeemsPhishy/nlp.py
@@ -3,6 +3,7 @@ from spacy.tokens import Doc
 import os
 
 #gewünschte spacy Modell wird geladen (Sprache: Englisch)
+#python -m spacy download en_core_web_sm (muss vor Ausführung installiert werden)
 nlp = spacy.load("en_core_web_lg")
 
 
@@ -24,23 +25,3 @@ for filename in os.listdir("C:\\Users\\Oliver.Wieder\\Jupyterlab\\NLP"):
     else:
         print("kein txt")
         continue
-'''file1 = open('examplefile.txt', 'r')
-Lines = file1.readlines()
-
-nlp = spacy.load("en_core_web_lg")
-
-count = 0
-# Strips the newline character
-for line in Lines:
-    count += 1
-    print("Line{}: {}".format(count, line.strip()))
-'''
-
-text = """spaCy is an open-source software library for advanced natural language processing, 
-written in the programming languages Python and Cython. The library is published under the MIT license
-and its main developers are Matthew Honnibal and Ines Montani, the founders of the software company Explosion."""
-doc = nlp(text)   
-
-print(doc.ents)   
-
-#python -m spacy download en_core_web_sm (muss vor Ausführung installiert werden)
