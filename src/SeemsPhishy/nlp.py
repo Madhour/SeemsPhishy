e8698703add31264a89436123f0297a4d1b1fd26
OWDSC <oliver@familie-wieder.de>
2021-12-07 17:12:26 +0100

    txt Dateien auslesen und Keywords extrahieren

--- src/SeemsPhishy/nlp.py
+++ src/SeemsPhishy/nlp.py
@@ -1,7 +1,40 @@
 import spacy
 from spacy.tokens import Doc
+import os
+
+#gewünschte spacy Modell wird geladen (Sprache: Englisch)
+nlp = spacy.load("en_core_web_lg")
+
+
+#alle files die im gegebenen directory liegen und mit .txt enden werden gelesen
+for filename in os.listdir("C:\\Users\\Oliver.Wieder\\Jupyterlab\\NLP"):
+    print("drinnen")
+    if filename.endswith(".txt"): 
+         # print(os.path.join(directory, filename))
+        file1 = open(filename, 'r')
+        Lines = file1.readlines() 
+
+        count = 0
+        # Strips the newline character
+        for line in Lines:
+            count += 1
+            print("Line{}: {}".format(count, line.strip()))
+            doc = nlp(line)   
+            print(doc.ents)
+    else:
+        print("kein txt")
+        continue
+'''file1 = open('examplefile.txt', 'r')
+Lines = file1.readlines()
 
 nlp = spacy.load("en_core_web_lg")
+
+count = 0
+# Strips the newline character
+for line in Lines:
+    count += 1
+    print("Line{}: {}".format(count, line.strip()))
+'''
 
 text = """spaCy is an open-source software library for advanced natural language processing, 
 written in the programming languages Python and Cython. The library is published under the MIT license
