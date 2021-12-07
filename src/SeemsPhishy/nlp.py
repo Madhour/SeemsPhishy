116e05009a19beb80ff8f3834b738536b88e023f
OWDSC <oliver@familie-wieder.de>
2021-12-07 13:07:47 +0100

    alte Sachen entfernen

--- src/SeemsPhishy/nlp.py
+++ src/SeemsPhishy/nlp.py
@@ -11,25 +11,3 @@ doc = nlp(text)
 print(doc.ents)   
 
 #python -m spacy download en_core_web_sm (muss vor Ausführung installiert werden)
-
-# import spacy
-# from spacy.tokens import Doc
-
-# if not Doc.has_extension("text_id"):
-#     Doc.set_extension("text_id", default=None)
-
-# text_tuples = [
-#     ("This is the first text.", {"text_id": "text1"}),
-#     ("This is the second text.", {"text_id": "text2"})
-# ]
-
-# nlp = spacy.load("en_core_web_sm")
-# doc_tuples = nlp.pipe(text_tuples, as_tuples=True)
-
-# docs = []
-# for doc, context in doc_tuples:
-#     doc._.text_id = context["text_id"]
-#     docs.append(doc)
-
-# for doc in docs:
-#     print(f"{doc._.text_id}: {doc.text}")
