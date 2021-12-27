15b39ca703c31dbd0df44dd287382e8f385a2b04
Madhour <aymanmadhour@gmail.com>
2021-12-27 15:36:17 +0100

    updated template

--- SeemsPhishy/textgen.py
+++ SeemsPhishy/textgen.py
@@ -1,24 +1,33 @@
 from transformers import pipeline
 from jinja2 import *
 import time
+import random
 
-def generate(input_text):
-    #generator1 = pipeline('text-generation', model='gpt2')
-    generator2 = pipeline('text-generation', model='distilgpt2') 
-    #generator1 = pipeline('text-generation', model='gpt2')
+random_names = (
+"Oskar Al-Ghazzawi",
+"Adzo Snider",
+"Georgios Yun",
+"Askr Ogtrop",
+"Winfrith McNeal",
+"Sven Bieber")
 
-    #output_text_1 = generator1(input_text, min_length=30, num_return_sequences=1)
-    output_text_2 = generator2(input_text, min_length=30, num_return_sequences=3)
+
+def generate(input_text, num_text = 1):
+    generator = pipeline('text-generation', model='gpt2') 
+
+    output_text = generator(input_text, min_length=100, num_return_sequences=num_text)
     
-    return output_text_2
+    return output_text
+
+
 
 def buildMail(text, organization):
     report_number = int(time.time())
-    organization = ""
-   
+    names = random.sample(random_names, (len(text)))
 
     template = Template(open('SeemsPhishy/templates/phishing_tpl.html').read()) # HTML Template stored inside /lib/
-    tpl = template.render(newsletter = text, report_number = report_number)
-    html = open(f'./SeemsPhishy/newsletters/Newsletter-{organization}-{report_number}.html', 'w') #stores rendered report as HTML file
+    tpl = template.render(newsletter = text, report_number = report_number, author_names = names)
+    #html = open(f'./SeemsPhishy/newsletters/Newsletter-{organization}-{report_number}.html', 'w') #stores rendered report as HTML file
+    html = open(f'./SeemsPhishy/newsletters/Newsletter-{organization}.html', 'w') #stores rendered report as HTML file
     html.write(tpl)
     html.close()
